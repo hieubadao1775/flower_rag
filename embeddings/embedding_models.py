dd6878dced4c28b0e562f982ec0cbce23b180b65
HieuHieu <145194277+hieubadao1775@users.noreply.github.com>
2026-02-22 14:25:33 +0700

    Update embedding_models.py

--- embeddings/embedding_models.py
+++ embeddings/embedding_models.py
@@ -1,12 +1,12 @@
-class SentenceTransformerEmbedding:
-    def __init__(self,
-                model_name="/home/hieu/Documents/Self Study/Khóa học chuyên chatbot/DỰ ÁN GIỮA KHÓA 01: RAG TƯ VẤN BÁN HOA/models/embedding_model/qwen3-0,6b",
-                local_files_only=True):
-        from sentence_transformers import SentenceTransformer
+# class SentenceTransformerEmbedding:
+#     def __init__(self,
+#                 model_name="/home/hieu/Documents/Self Study/Khóa học chuyên chatbot/DỰ ÁN GIỮA KHÓA 01: RAG TƯ VẤN BÁN HOA/models/embedding_model/qwen3-0,6b",
+#                 local_files_only=True):
+#         from sentence_transformers import SentenceTransformer
 
-        self.client = SentenceTransformer(model_name, local_files_only=local_files_only)
-    def encode(self, docs: list[str]) -> list[list]:
-        return self.client.encode(docs).tolist()
+#         self.client = SentenceTransformer(model_name, local_files_only=local_files_only)
+#     def encode(self, docs: list[str]) -> list[list]:
+#         return self.client.encode(docs).tolist()
 
 class GeminiEmbedding:
     def __init__(self, model_name="gemini-embedding-001"):
